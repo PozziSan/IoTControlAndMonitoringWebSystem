b6cbfb25dd2772345bfd262b2a39bf40d6331943
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2019-11-04 00:29:00 -0200

    Creates Table Views For Measures

--- webApp/urls.py
+++ webApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('turn_led_on', views.turn_led_on, name="turn_led_on")
+    path('turn_led_on', views.turn_led_on, name="turn_led_on"),
+    path('temperature_measures', views.temperature_measures, name='temperature_measures'),
+    path('humidity_measures', views.humidity_measures, name='humidity_measures')
 ]
--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from .services import get_mqtt
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+from .services import get_mqtt
+
+from .models import TemperatureMeasures
+from .models import HumidityMeasures
 
 
 # Create your views here.
@@ -14,3 +19,23 @@ def turn_led_on(request):
     mqtt.publish_message('webApp/actuator', '1')
 
     return HttpResponse(status=200)
+
+
+def temperature_measures(request):
+    temperature_measures_list = TemperatureMeasures.objects.all().order_by('-created_at')
+
+    paginator = Paginator(temperature_measures_list, 30)
+    page = request.GET.get('page')
+    measures = paginator.get_page(page)
+
+    return render(request, 'webApp/temperature_measures.html', {'temperature_measures': measures})
+
+
+def humidity_measures(request):
+    humidity_measures_list = HumidityMeasures.objects.all().order_by('-created_at')
+
+    paginator = Paginator(humidity_measures_list, 30)
+    page = request.GET.get('page')
+    measures = paginator.get_page(page)
+
+    return render(request, 'webApp/humidity_measures.html', {'humidity_measures': measures})

f2078112549399353f20c6e14f5b6f90d86ba3cc
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2019-11-04 01:52:16 -0200

    Adds Charts and Actuators Views
    Temperature and Humidity Measures Views was added using ChartJS library.
    
    Also, an actuator view was created

--- webApp/urls.py
+++ webApp/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('turn_led_on', views.turn_led_on, name="turn_led_on"),
+    path('', views.actuators, name='actuators'),
+    path('blink_led', views.blink_led, name="blink_led"),
     path('temperature_measures', views.temperature_measures, name='temperature_measures'),
-    path('humidity_measures', views.humidity_measures, name='humidity_measures')
+    path('humidity_measures', views.humidity_measures, name='humidity_measures'),
+    path('temperature_chart', views.temperature_chart, name='temperature_chart'),
+    path('humidity_chart', views.humidity_chart, name='humidity_chart'),
 ]
--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from json import dumps
 
 from .services import get_mqtt
 
@@ -9,11 +10,11 @@ from .models import HumidityMeasures
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'webApp/index.html')
+def actuators(request):
+    return render(request, 'webApp/actuators.html')
 
 
-def turn_led_on(request):
+def blink_led(request):
     mqtt = get_mqtt()
 
     mqtt.publish_message('webApp/actuator', '1')
@@ -39,3 +40,33 @@ def humidity_measures(request):
     measures = paginator.get_page(page)
 
     return render(request, 'webApp/humidity_measures.html', {'humidity_measures': measures})
+
+
+def temperature_chart(request):
+    temperature_measures = TemperatureMeasures.objects.all().order_by('-created_at')[:50]
+
+    temperatures = [float(temperature_measure.measure) for temperature_measure in temperature_measures]
+    times = [temperature_measure.created_at.strftime('%Y-%m-%d %H:%M:%S') for temperature_measure in
+             temperature_measures]
+
+    context = {
+        'temperatures': dumps(temperatures),
+        'times': dumps(times)
+    }
+
+    return render(request, 'webApp/temperature_chart.html', context)
+
+
+def humidity_chart(request):
+    humidity_measures = HumidityMeasures.objects.all().order_by('-created_at')[:50]
+
+    humidities = [float(humidity_measure.measure) for humidity_measure in humidity_measures]
+    times = [humidity_measure.created_at.strftime('%Y-%m-%d %H:%M:%S') for humidity_measure in
+             humidity_measures]
+
+    context = {
+        'temperatures': dumps(humidities),
+        'times': dumps(times)
+    }
+
+    return render(request, 'webApp/temperature_chart.html', context)

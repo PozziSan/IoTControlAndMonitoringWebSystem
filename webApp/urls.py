0c05e8eaa4ba03d519b0b844f3c5b3e953b69f18
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2019-11-03 23:38:07 -0200

    Adds Test Button on View to  MQTT Publishing

--- webApp/urls.py
+++ webApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('turn_led_on', views.turn_led_on, name="turn_led_on")
 ]
--- webApp/views.py
+++ webApp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .services import get_mqtt
 from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
     return render(request, 'webApp/index.html')
+
+
+def turn_led_on(request):
+    mqtt = get_mqtt()
+
+    mqtt.publish_message('webApp/actuator', '1')
+
+    return HttpResponse(status=200)
